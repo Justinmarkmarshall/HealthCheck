af59396b638549176de74d79e619f6cc2ac45bb2
Justin Marshall <justinmarkmarshall1@gmail.com>
2021-08-04 21:32:41 +0100

    add the health checks

--- HealthCheckDemo/Startup.cs
+++ HealthCheckDemo/Startup.cs
@@ -1,16 +1,21 @@
 using HealthCheckDemo.Data;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//into to health checks in .net core tim corey
+//https://www.youtube.com/watch?v=Kbfto6Y2xdw
 namespace HealthCheckDemo
 {
     public class Startup
@@ -28,6 +33,24 @@ namespace HealthCheckDemo
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            //Configure Health Checks
+            services.AddHealthChecks()
+                //start by hard coding in a test health check and a response message
+                .AddCheck("Foo service", () =>
+                //HealthCheckResult.Healthy("The check of the foo service worked."));
+                // change to this for the result to be unhealthy
+                //HealthCheckResult.Unhealthy("The check of the foo service resulted in unhealthy"));
+                // change to this for the result to be degraded - there's aproblem, but it's still working
+                //we can also have tags which we can use to filter which checks run
+                //although this is a simple lambda expression just to return degraded, in actual fact I can run any custom health check here, 
+                // and return degraded if it means that the project is up, but just not running optimally.
+                HealthCheckResult.Degraded("We're working but suboptimal - dependent on requirements"), new[] { "service" })
+                //we can actually put these into different checks, will more than one it will return the worst one
+                // however we can select which ones we want to prioritise using tags, maybe buzz service is less important than foo bar services
+                .AddCheck("bar service", () => HealthCheckResult.Healthy("the bar service is healthy"), new[] { "service" })
+                .AddCheck("buzz service", () => HealthCheckResult.Unhealthy("the buzz service is f**ked"), new[] { "sql" });
+
             services.AddSingleton<WeatherForecastService>();
         }
 
@@ -52,6 +75,31 @@ namespace HealthCheckDemo
 
             app.UseEndpoints(endpoints =>
             {
+                //map the health check to the health endpoint - change this to give us more information
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions() 
+                {
+                    //format a json response for our health check
+                    //whole object of information could be used to create
+                    //a dedicated application
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
+                //create a different health check on a different endpoint
+                endpoints.MapHealthChecks("/health/quick", new HealthCheckOptions()
+                {
+                    //could call a method, but in this case will just return false, which means it just does the basic healkth check
+                    Predicate = _ => false
+                });
+
+                //use tag to just call the services healthchecks
+                endpoints.MapHealthChecks("/health/services", new HealthCheckOptions()
+                {
+                    //runs health checks with the right tags
+                    Predicate = reg => reg.Tags.Contains("service"),
+                    //give me the full JSON
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
